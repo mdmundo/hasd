c55a9196cc52bc0e3a297ead3c4eb528351e3c5c
Edmundo Paulino <manyymoore@gmail.com>
2020-12-29 20:24:39 -0300

    Updates

---
+++ src/Presenter/hymns/index.js
@@ -0,0 +1,3 @@
+import _1 from "./001.json";
+
+export default [_1];
--- src/Presenter/index.js
+++ src/Presenter/index.js
@@ -1,13 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import context from '../context';
+import hymns from './hymns';
 
 const App = () => {
   const { state } = useContext(context);
 
+  const hymn = hymns[state.hymn.number - 1];
+
+  const [nextUpdate, setNextUpdate] = useState(0);
+  const [current, setCurrent] = useState(-1);
+
+  useEffect(() => {
+    if (hymn.text[nextUpdate]?.attributes.show === state.timer) {
+      setCurrent(nextUpdate);
+      setNextUpdate(nextUpdate + 1);
+    }
+  }, [state.timer, nextUpdate, hymn.text]);
+
   return (
     <div>
       <p>number: {state.hymn.number}</p>
+      <p>current: {current}</p>
+      {current < 0 ? (
+        <pre>
+          {`${parseInt(hymn.attributes.number, 10)}
+${hymn.attributes.title}
+${hymn.attributes.author}
+${hymn.attributes.verse}`}
+        </pre>
+      ) : (
+        <pre>{hymn.text[current].text}</pre>
+      )}
     </div>
   );
 };
+
 export default App;
