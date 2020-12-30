96ee2baf2f7fa4f743c39b919736716d42b09ae3
Edmundo Paulino <manyymoore@gmail.com>
2020-12-30 10:30:25 -0300

    Update maxAge

--- src/Show/Player/cache.js
+++ src/Show/Player/cache.js
@@ -3,9 +3,14 @@ import {
   cacheAdapterEnhancer,
   throttleAdapterEnhancer
 } from 'axios-extensions';
+import LRUCache from 'lru-cache';
 
 const http = axios.create({
-  adapter: throttleAdapterEnhancer(cacheAdapterEnhancer(axios.defaults.adapter))
+  adapter: throttleAdapterEnhancer(
+    cacheAdapterEnhancer(axios.defaults.adapter, {
+      defaultCache: new LRUCache({ maxAge: 86400000, max: 0 })
+    })
+  )
 });
 
 export default http;
