4bb5573118f7f592c7067922f1e06f437ac1fc40
Edmundo Paulino <manyymoore@gmail.com>
2021-01-18 15:59:50 -0300

    Updates fix bug

--- src/Show/Presenter/favorites.js
+++ src/Show/Presenter/favorites.js
@@ -2,7 +2,11 @@ import localforage from '../../favorites-storage';
 
 export const setFavorite = async (newFavorite) => {
   const favorites = await localforage.getItem('favorites');
-  favorites.push(newFavorite);
 
-  await localforage.setItem('favorites', Array.from(new Set(favorites)));
+  if (favorites !== null) {
+    favorites.push(newFavorite);
+    await localforage.setItem('favorites', Array.from(new Set(favorites)));
+  } else {
+    await localforage.setItem('favorites', [newFavorite]);
+  }
 };
