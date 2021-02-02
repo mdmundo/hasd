9bee5c6c74c6d99fe9ad28cd31181ee40c40e3ae
Edmundo Paulino <manyymoore@gmail.com>
2021-02-02 16:19:59 -0300

    remove unused

--- src/Main/Chooser/index.js
+++ src/Main/Chooser/index.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Grid from '@material-ui/core/Grid';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
