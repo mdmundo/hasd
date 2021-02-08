1828231d4f057a9a6978fbea6a6c68423a931acc
Edmundo Paulino <manyymoore@gmail.com>
2021-02-08 11:27:56 -0300

    refactor: Remove unused files

--- src/Show/Player/timer.js
+++
@@ -1,5 +0,0 @@
-export const convert = (time) => {
-  const minutes = Math.floor(time / 60);
-  const seconds = Math.floor(time % 60);
-  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
-};
